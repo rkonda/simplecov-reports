e6d5b20f20a14f626dd19c2fce1558450adbdbae
AuthorTesterOne <AuthorTesterOne@simplecov.com>
2013-06-28 15:36:49 -0700

    Author test fixture: minor fix

--- test/fixtures/author_report/best_author_tolerance.rb
+++ test/fixtures/author_report/best_author_tolerance.rb
@@ -9,17 +9,17 @@ puts "Rajesh 8"
 puts "Rajesh 9"
 puts "Rajesh 10"
 
-if author=="AuthorTesterOne"
-  if coverage=="cover"
-    puts "Author-1 101"
-    puts "Author-1 102"
-    puts "Author-1 103"
-    puts "Author-1 104"
-    puts "Author-1 105"
-    puts "Author-1 106"
-    puts "Author-1 107"
-  else
-    puts "Author-1 108"
-  end
+# author=="AuthorTesterOne"
+if coverage=="cover"
+  puts "Author-1 101"
+  puts "Author-1 102"
+  puts "Author-1 103"
+  puts "Author-1 104"
+  puts "Author-1 105"
+  puts "Author-1 106"
+  puts "Author-1 107"
+  puts "Author-1 108"
+else
+  puts "Author-1 109"
 end
 
