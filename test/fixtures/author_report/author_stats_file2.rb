186d2a0ef87c1d27d9bfaa9deed4a834bb82dfc1
AuthorTesterOne <AuthorTesterOne@simplecov.com>
2013-06-28 20:19:40 -0700

    Author stats fixture: AuthorTesterOne commits to file2

---
+++ test/fixtures/author_report/author_stats_file2.rb
@@ -0,0 +1,14 @@
+# author=="AuthorTesterOne"
+# (1/10) 10% coverage
+if COVERAGE == "cover"
+else
+  line = "Author-1 1"
+  line = "Author-1 2"
+  line = "Author-1 3"
+  line = "Author-1 4"
+  line = "Author-1 5"
+  line = "Author-1 6"
+  line = "Author-1 7"
+  line = "Author-1 8"
+  line = "Author-1 9"
+end
