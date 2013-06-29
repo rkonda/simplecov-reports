f9001e168dc3cb7fbd4f0a4fbe1f9d6632ca4adf
Rajesh Konda <rkonda@ooyala.com>
2013-06-28 19:18:24 -0700

    Remove SimpleCov.start from helper.rb

--- test/helper.rb
+++ test/helper.rb
@@ -13,27 +13,7 @@ class SimpleCov::Formatter::MergedFormatter
   end
 end
 
-def start_simplecov
-  SimpleCov.formatter = SimpleCov::Formatter::MergedFormatter
-
-  SimpleCov.start do
-    use_merging true
-    merge_timeout 3600
-
-    add_report :type => SimpleCov::Configuration::ReportTypes::ItemsMissingCoverage::Api
-    add_report :type => SimpleCov::Configuration::ReportTypes::ItemsMissingCoverage::Class
-    add_report :type => SimpleCov::Configuration::ReportTypes::ItemsMissingCoverage::Method
-    add_report :type => SimpleCov::Configuration::ReportTypes::ItemsMissingCoverage::Configure
-    add_report(:type => SimpleCov::Configuration::ReportTypes::Author,
-               :sub_types => [
-                 SimpleCov::Configuration::ReportTypes::Author::BestAuthor,
-                 SimpleCov::Configuration::ReportTypes::Author::AuthorStats
-               ],
-               :best_authors_count => 4)
-  end
-end
-
-start_simplecov
+SimpleCov.formatter = SimpleCov::Formatter::MergedFormatter
 
 
 
--- test/test_file_reports.rb
+++ test/test_file_reports.rb
@@ -1,5 +1,21 @@
 require_relative 'helper'
 
+SimpleCov.start do
+  use_merging true
+  merge_timeout 3600
+
+  add_report :type => SimpleCov::Configuration::ReportTypes::ItemsMissingCoverage::Api
+  add_report :type => SimpleCov::Configuration::ReportTypes::ItemsMissingCoverage::Class
+  add_report :type => SimpleCov::Configuration::ReportTypes::ItemsMissingCoverage::Method
+  add_report :type => SimpleCov::Configuration::ReportTypes::ItemsMissingCoverage::Configure
+  add_report(:type => SimpleCov::Configuration::ReportTypes::Author,
+             :sub_types => [
+               SimpleCov::Configuration::ReportTypes::Author::BestAuthor,
+               SimpleCov::Configuration::ReportTypes::Author::AuthorStats
+             ],
+             :best_authors_count => 3)
+end
+
 # Execute code to get coverage
 
 require_relative "fixtures/file_report/classes_covered"
